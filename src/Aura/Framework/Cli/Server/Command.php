ce0a33863fedb215d7a7dfb0de49e14745040762
Galactic Void <galacticvoid+git@gmail.com>
2012-03-05 14:49:11 +0000

    Quickly setup and run a development server.
    This script has the benefit of getting around a bug in the PHP 5.4 web
    server that causes `PATH_INFO` not to be set if there is a dot in the
    path. https://bugs.php.net/bug.php?id=61286
    
    To run execute in a console `php php package/Aura.Framework/cli/server`
    The default port is 8000, to change the port add the flag  `-p
    <port_number>`.

---
+++ src/Aura/Framework/Cli/Server/Command.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * 
+ * This file is part of the Aura project for PHP.
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
+namespace Aura\Framework\Cli\Server;
+
+use Aura\Framework\Cli\AbstractCommand;
+use Aura\Cli\Option;
+
+/**
+ * 
+ * Setup and run a development server.
+ * 
+ * @package Aura.Framework
+ * 
+ */
+class Command extends AbstractCommand
+{
+    /**
+     * 
+     * @var string The path to the PHP executable.
+     * 
+     */
+    protected $php = 'php';
+
+    protected $options = [
+        'port' => [
+            'long'    => 'port',
+            'short'   => 'p',
+            'param'   => Option::PARAM_REQUIRED,
+            'multi'   => false,
+            'default' => 8000,
+        ],
+    ];
+
+    /**
+     *
+     * Set the path to the PHP executable.
+     *
+     * @param string $path The path to PHP.
+     *
+     *
+     */
+    public function phpPath($path)
+    {
+        $this->php = $path;
+    }
+
+    /**
+     * 
+     * Setup and run the server.
+     * 
+     * @return void
+     * 
+     */
+    public function action()
+    {
+        $url = "http://localhost:{$this->getopt->port}/";
+        $msg = "Starting the Aura development server @ {$url}";
+
+        $this->stdio->outln($msg);
+
+        $root = substr(__DIR__, 0, strrpos(__DIR__, 'package'));
+        $root = $root . 'web';
+
+        // change to the web root directory
+        chdir($root);
+
+        $router = __DIR__ . DIRECTORY_SEPARATOR . 'router.php';
+        $cmd    = "{$this->php} -S 0.0.0.0:{$this->getopt->port} {$router}";
+        $pipe   = popen($cmd, 'r');
+
+        while (! feof($pipe)) {
+            $this->stdio->outln(fread($pipe, 2048));
+        }
+
+        pclose($pipe);
+    }
+}
---
+++ src/Aura/Framework/Cli/Server/router.php
@@ -0,0 +1,27 @@
+<?php
+
+$root      = substr(__DIR__, 0, strrpos(__DIR__, 'package'));
+$root      = $root . 'web';
+$has_index = strpos(strtolower($_SERVER['REQUEST_URI']), '/index.php');
+
+if (false === $has_index && file_exists($root . $_SERVER['REQUEST_URI'])) {
+      return false;
+}
+
+// workaround for PHP web server issue
+// https://bugs.php.net/bug.php?id=61286
+if (false !== strpos($_SERVER['REQUEST_URI'], '.')) {
+    if (false === $has_index) {
+        $path = $_SERVER['REQUEST_URI'];
+    } else {
+        $path = substr($_SERVER['REQUEST_URI'], $has_index + 10);
+    }
+
+    if (false === ($end = strpos($path, '?'))) {
+        $_SERVER['PATH_INFO'] = $path;
+    } else {
+        $_SERVER['PATH_INFO'] = substr($path, 0, $end);
+    }
+}
+
+require_once 'index.php';
