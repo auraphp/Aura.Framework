auraphp/Aura.Router

commit eaf6c1188dc46e123da9367e0a5e59d9a9bd4079

[CFG] Remove 'router_map' service; leave this for the framework-level config

config/default.php
@@ -5,11 +5,6 @@
 $loader->add('Aura\Router\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 
 /**
- * Services
- */
-$di->set('router_map', $di->lazyNew('Aura\Router\Map'));
-
-/**
  * Aura\Router\Map
  */
 $di->params['Aura\Router\Map'] = [

auraphp/Aura.Framework

commit 349e66da66bf2d4941562e3c4558def6f0b58637

Fixes the issues introduced in https://github.com/auraphp/Aura.Router/commit/eaf6c1188dc46e123da9367e0a5e59d9a9bd4079

config/default.php
@@ -9,6 +9,7 @@ $loader->add('Aura\Framework\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src')
  */
 $di->set('framework_inflect', $di->lazyNew('Aura\Framework\Inflect'));
 $di->set('web_front', $di->lazyNew('Aura\Framework\Web\Controller\Front'));
+$di->set('router_map', $di->lazyNew('Aura\Router\Map'));
 // override the service for signal manager
 $di->set('signal_manager', $di->lazyNew('Aura\Framework\Signal\Manager'));
 
@@ -39,7 +40,7 @@ $di->setter['Aura\Framework\Cli\CacheConfig\Command']['setSystem'] = $di->lazyGe
  */
 $di->params['Aura\Framework\Cli\Factory']['forge'] = $di->getForge();
 $cli_path = $system . DIRECTORY_SEPARATOR . 'package' . DIRECTORY_SEPARATOR .
-			'Aura.Framework' . DIRECTORY_SEPARATOR . 'cli' . DIRECTORY_SEPARATOR;
+      'Aura.Framework' . DIRECTORY_SEPARATOR . 'cli' . DIRECTORY_SEPARATOR;
 $di->params['Aura\Framework\Cli\Factory']['map'][$cli_path . 'cache-classmap'] = 'Aura\Framework\Cli\CacheClassmap\Command';
 $di->params['Aura\Framework\Cli\Factory']['map'][$cli_path . 'cache-config'] = 'Aura\Framework\Cli\CacheConfig\Command';
 $di->params['Aura\Framework\Cli\Factory']['map'][$cli_path . 'server'] = 'Aura\Framework\Cli\Server\Command';